c27dfd13c8fa7935f7f25c27d432fd9213c14fc1
dcadsc <dcadsc@126.com>
2025-03-21 09:09:57 +0800

    websocket掉线重连机制

--- src/main/java/com/example/WebSocketClientImpl.java
+++ src/main/java/com/example/WebSocketClientImpl.java
@@ -4,18 +4,25 @@ import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 
 import java.net.URI;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class WebSocketClientImpl extends WebSocketClient {
     private MessageProcessor processor;
+    private URI serverUri;
+    private ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
 
     public WebSocketClientImpl(URI serverUri, MessageProcessor processor) {
         super(serverUri);
+        this.serverUri = serverUri;
         this.processor = processor;
     }
 
     @Override
     public void onOpen(ServerHandshake handshakedata) {
         System.out.println("[WebSocket] Connected to server");
+
         // 连接成功后，发送确认消息
         String confirmMessage = "Connection established with server";
         this.send(confirmMessage);
@@ -28,11 +35,25 @@ public class WebSocketClientImpl extends WebSocketClient {
 
     @Override
     public void onClose(int code, String reason, boolean remote) {
-        System.out.println("[WebSocket] Connection closed");
+        System.out.println("[WebSocket] Connection closed. Reason: " + reason);
+        scheduleReconnect();
     }
 
     @Override
     public void onError(Exception ex) {
-        ex.printStackTrace();
+        System.out.println("[WebSocket] Error occurred: " + ex.getMessage());
+        scheduleReconnect();
+    }
+
+    private void scheduleReconnect() {
+        scheduler.schedule(() -> {
+            System.out.println("[WebSocket] Attempting to reconnect...");
+            try {
+                WebSocketClientImpl newClient = new WebSocketClientImpl(serverUri, processor);
+                newClient.connect();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }, 5, TimeUnit.SECONDS);
     }
 }
